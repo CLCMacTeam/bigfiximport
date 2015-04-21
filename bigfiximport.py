c0ea6f5c634ddacf6da5841ad4c61658bcb2a286
Matt Hansen <mah60@psu.edu>
2015-04-21 17:07:06 -0400

    Added hachoir and flat package

--- bigfiximport.py
+++ bigfiximport.py
@@ -14,6 +14,7 @@ import os
 import re
 import sys
 import shutil
+import string
 import argparse
 import getpass
 import zipfile
@@ -45,7 +46,6 @@ from types import ModuleType
 from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('templates'))
 
-
 # -----------------------------------------------------------------------------
 # Variables
 # -----------------------------------------------------------------------------
@@ -61,6 +61,12 @@ MUNKILIB_PATH = os.path.join('munki-master', 'code', 'client', 'munkilib')
 parser = argparse.ArgumentParser(description='bigfiximport')
 parser.add_argument('-v', '--verbose', action='count', dest='verbosity',
                     help='increase output verbosity', default=0)
+parser.add_argument('--adobe', action='store_true', default=False,
+                    help='process an Adobe CC update file')
+parser.add_argument('--copyfromdmg', action='store_true', default=False,
+                    help='process an OS X copy from dmg installer')
+parser.add_argument('--package', action='store_true', default=False,
+                    help='process an OS X package installer')
 parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
 
 args, extra_args = parser.parse_known_args()
@@ -88,6 +94,16 @@ try:
     besapi_version = pkg_resources.get_distribution("besapi").version
 except ImportError:
     BESAPI_AVAILABLE = False
+
+try:
+    import hachoir_core
+    import hachoir_core.cmd_line
+    import hachoir_metadata
+    import hachoir_parser
+    HACHOIR_AVAILABLE = True
+    hachoir_version = pkg_resources.get_distribution("hachoir_core").version
+except ImportError:
+    HACHOIR_AVAILABLE = False
 
 # Used to ignore some import errors
 class DummyModule(ModuleType):
@@ -121,11 +137,13 @@ if os.path.isdir('munkilib'):
 
 # Verbose environment output
 if args.verbosity > 1:
-    for p in ['PLATFORM', 'BESAPI_AVAILABLE', 'MUNKILIB_AVAILABLE', 'DARWIN_FOUNDATION_AVAILABLE']:
+    for p in ['PLATFORM', 'BESAPI_AVAILABLE', 'MUNKILIB_AVAILABLE', 'DARWIN_FOUNDATION_AVAILABLE', 'HACHOIR_AVAILABLE']:
         print "%s: %s" % (p, eval(p))
 
     if BESAPI_AVAILABLE:
         print "besapi version: %s" % besapi_version
+    if HACHOIR_AVAILABLE:
+        print "hachoir_core version: %s" % hachoir_version
     if MUNKILIB_AVAILABLE:
         print "munkilib version: %s" % munkilib_version
 
@@ -269,161 +287,240 @@ def getBundleInfo(path):
             pass
 
     return None
+    
+def getHachoirMetaData(file_path):
+
+    ufilepath = hachoir_core.cmd_line.unicodeFilename(str(file_path))
+    parser = hachoir_parser.createParser(ufilepath, file_path)
+    
+    if not parser:
+        print "Unable to parse file metadata"
+        sys.exit(1)
+
+    try:
+        metadata = hachoir_metadata.extractMetadata(parser)
+    except HachoirError, err:
+        print "Metadata extraction error: %s" % unicode(err)
+        sys.exit(1)
+        
+    if not metadata:
+        print "Unable to extract metadata"
+        sys.exit(1)
+    else:
+        return metadata
 
 # -----------------------------------------------------------------------------
 # Core
 # -----------------------------------------------------------------------------
 if args.verbosity > 1:
-    print 'Number of arguments:', len(sys.argv), 'arguments.'
+    print '\nNumber of arguments:', len(sys.argv), 'arguments.'
     print 'Argument List:', str(sys.argv)
 
 file_path = sys.argv[-1]
 file_path_noextension = os.path.splitext(file_path)[0]
 file_mime, file_encoding = guess_file_type(file_path)
 file_is_local = True if os.path.isfile(file_path) else False
+file_name_isfolder = os.path.isdir(file_path)
 
 file_name = os.path.basename(file_path)
 file_name_noextension, file_extension = os.path.splitext(file_name)
 base_file_name = file_name.split('-')[0].split('.')[0]
 
-if 'adobe' in file_path.lower():
-    IS_ADOBE_UPDATE = True
-else:
-    IS_ADOBE_UPDATE = False
-
 # -----------------------------------------------------------------------------
 # OS X Drag & Drop App
 # -----------------------------------------------------------------------------
-if file_mime == 'application/x-apple-diskimage' and file_is_local and DARWIN_FOUNDATION_AVAILABLE and not IS_ADOBE_UPDATE:
+if file_mime == 'application/x-apple-diskimage' and file_is_local and DARWIN_FOUNDATION_AVAILABLE and not args.adobe:
     template = env.get_template('copyfromdmg.bes')
     mountpoints = munkicommon.mountdmg(file_path, use_existing_mounts=True)
 
-    for (itemname, dummy_dirs, dummy_files) in os.walk(mountpoints[0]):
-        itempath = os.path.join(mountpoints[0], itemname)
-        if munkicommon.isApplication(itempath):
-            item = itemname
-            iteminfo = getiteminfo(itempath)
-            if iteminfo:
-                break
+    iteminfo = ''
+    try:
+        for (itemname, dummy_dirs, dummy_files) in os.walk(mountpoints[0]):
+            itempath = os.path.join(mountpoints[0], itemname)
+            if munkicommon.isApplication(itempath):
+                item = itemname
+                iteminfo = getiteminfo(itempath)
+                if iteminfo:
+                    break
+    
+        if iteminfo:
+            if os.path.isabs(item):
+                mountpointPattern = "^%s/" % mountpoints[0]
+                item = re.sub(mountpointPattern, '', item)
+    
+            cataloginfo = {}
+            cataloginfo['display_name'] = iteminfo.get('CFBundleName',
+                                            os.path.splitext(item)[0])
+            version_comparison_key = iteminfo.get(
+                'version_comparison_key', "CFBundleShortVersionString")
+            cataloginfo['version'] = \
+                iteminfo.get(version_comparison_key, "0")
+            cataloginfo.update(iteminfo)
+            cataloginfo['item_to_copy'] = item
+            cataloginfo['base_file_name'] = base_file_name
+            cataloginfo.update(get_sha1_size(file_path))
+            cataloginfo.update(get_env_source_mime_data())
+            
+            # Render and POST new task to console site
+            new_task = B.post('tasks/custom/SysManDev', template.render(**cataloginfo))
+    except:
+        print "Unable to read application data from disk image!"
+        sys.exit(1)
+    finally:
+        #eject the dmg
+        munkicommon.unmountdmg(mountpoints[0])
 
-    if iteminfo:
-        if os.path.isabs(item):
-            mountpointPattern = "^%s/" % mountpoints[0]
-            item = re.sub(mountpointPattern, '', item)
+# -----------------------------------------------------------------------------
+# OS X Flat Installer Package (.pkg)
+# -----------------------------------------------------------------------------
+if file_mime == 'application/octet-stream' and file_extension == '.pkg' and args.package:
 
-        cataloginfo = {}
-        cataloginfo['display_name'] = iteminfo.get('CFBundleName',
-                                        os.path.splitext(item)[0])
-        version_comparison_key = iteminfo.get(
-            'version_comparison_key', "CFBundleShortVersionString")
-        cataloginfo['version'] = \
-            iteminfo.get(version_comparison_key, "0")
-        cataloginfo.update(iteminfo)
-        cataloginfo['item_to_copy'] = item
-        cataloginfo['base_file_name'] = base_file_name
-        cataloginfo.update(get_sha1_size(file_path))
-        cataloginfo.update(get_env_source_mime_data())
+    template = env.get_template('appleflatpackageinstaller.bes')
+    pkginfo = munkicommon.getPackageMetaData(file_path)
+    
+    pkginfo.update(get_sha1_size(file_path))
+    pkginfo.update(get_env_source_mime_data())
+    pkginfo['base_file_name'] = base_file_name
+    
+    # Render and POST new task to console site
+    #new_task = B.post('tasks/custom/SysManDev', template.render(**pkginfo))
+    print template.render(**pkginfo)
+    
+# -----------------------------------------------------------------------------
+# OS X Bundle Installer Package (.mpkg)
+# -----------------------------------------------------------------------------
+if file_name_isfolder and file_extension == '.mpkg' and args.package:
+    pass
+    
 
-    #eject the dmg
-    munkicommon.unmountdmg(mountpoints[0])
-    new_task = B.post('tasks/custom/SysManDev', template.render(**cataloginfo))
+# -----------------------------------------------------------------------------
+# OS X Package in a Disk Image
+# -----------------------------------------------------------------------------
+if file_mime == 'application/x-apple-diskimage' and file_is_local and DARWIN_FOUNDATION_AVAILABLE and args.package:
+    pass
+    
+# -----------------------------------------------------------------------------
+# Windows MSI
+# -----------------------------------------------------------------------------
+if file_mime == 'application/x-msdownload' and file_extension == '.msi':
+    pass
+
+# -----------------------------------------------------------------------------
+# Windows EXE
+# -----------------------------------------------------------------------------
+mimeinfo = {}
+if HACHOIR_AVAILABLE and file_mime == 'application/x-msdownload' and file_extension == '.exe':
+    for data_item in getHachoirMetaData(file_path):
+        for value in data_item.values:
+            mimeinfo[data_item.key] = filter(lambda x: x in string.printable, value.text)
+
+    #print mimeinfo
+    with zipfile.ZipFile(file_path, 'r') as exe:
+       print_zip_info(exe)
+    
 
 # -----------------------------------------------------------------------------
 # Adobe Updates
 # -----------------------------------------------------------------------------
 
-# Mac Adobe Update (.dmg)
-if file_mime == 'application/x-apple-diskimage' and file_is_local and DARWIN_FOUNDATION_AVAILABLE and IS_ADOBE_UPDATE:
-    template = env.get_template('ccupdatemacosx.bes')
-    mounts = adobeutils.mountAdobeDmg(file_path)
+if args.adobe:
 
-    for mount in mounts:
-        adobe_info = adobeutils.getAdobeSetupInfo(mount)
-        adobe_info['adobepatchinstaller'] = adobeutils.findAdobePatchInstallerApp(mount)
-
-        mountpointPattern = "^%s/" % mount
-        adobepatchinstaller = re.sub(mountpointPattern, '', adobe_info['adobepatchinstaller'])
-
-        for (path, dummy_dirs, dummy_files) in os.walk(mount):
-            if path.endswith('/payloads'):
-                payloads_dir = path
-
-        with open(os.path.join(payloads_dir, 'UpdateManifest.xml'), 'r') as setupfile:
-            root = ET.parse(setupfile).getroot()
+    # Mac Adobe Update (.dmg)
+    if file_mime == 'application/x-apple-diskimage' and file_is_local and DARWIN_FOUNDATION_AVAILABLE:
+        template = env.get_template('ccupdatemacosx.bes')
+        mounts = adobeutils.mountAdobeDmg(file_path)
+    
+        try:
+            for mount in mounts:
+                adobe_info = adobeutils.getAdobeSetupInfo(mount)
+                adobe_info['adobepatchinstaller'] = adobeutils.findAdobePatchInstallerApp(mount)
+        
+                mountpointPattern = "^%s/" % mount
+                adobepatchinstaller = re.sub(mountpointPattern, '', adobe_info['adobepatchinstaller'])
+    
+                for (path, dummy_dirs, dummy_files) in os.walk(mount):
+                    if path.endswith('/payloads'):
+                        payloads_dir = path
+        
+                with open(os.path.join(payloads_dir, 'UpdateManifest.xml'), 'r') as setupfile:
+                    root = ET.parse(setupfile).getroot()
+                    adobe_info['description'] = root.find('''.//Description/en_US''').text.replace(u'\xa0', u' ')
+        except:
+            print "Unable to find information in Adobe CC Update disk image!"
+            sys.exit(1)
+        finally:    
+            munkicommon.unmountdmg(mount)
+    
+    # Windows Adobe Update (.zip)
+    elif file_mime == 'application/zip' and file_is_local:
+        
+        # Pick template based on '64bit' or '32bit' in file_path
+        if any(x in file_path for x in ['64Bit', '64bit', 'X64', 'x64']):
+            template = env.get_template('ccupdatewindows64.bes')
+        elif any(x in file_path for x in ['32Bit', '32bit']):
+            template = env.get_template('ccupdatewindows32.bes')
+        else:
+            template = env.get_template('ccupdatewindows.bes')
+    
+        zf = zipfile.ZipFile(file_path, 'r')
+        extractdir = os.path.join(tempfile.gettempdir(), file_name_noextension)
+    
+        for name in zf.namelist():
+            if not name.endswith('.zip') and not name.endswith('.exe'):
+                if name.endswith('Setup.xml') or name.endswith('setup.xml'):
+                    setup_xml = name
+                elif name.endswith('UpdateManifest.xml'):
+                    update_manifest = name
+                
+                (dirname, filename) = os.path.split(name)
+                zf.extract(name, extractdir)
+    
+        adobe_info = adobeutils.getAdobeSetupInfo(extractdir)
+        adobe_info['adobepatchinstaller'] = 'AdobePatchInstaller.exe'
+    
+        try:
+            with open(os.path.join(extractdir, setup_xml), 'r') as setupfile:
+                root = ET.parse(setupfile).getroot()
+                adobe_info['display_name'] = root.find('''.//Media/Volume/Name''').text
+        except AttributeError:
+            pass # Can't find display name, so we'll get it from UpdateManifest next
+        
+        with open(os.path.join(extractdir, update_manifest), 'r') as manifestfile:
+            root = ET.parse(manifestfile).getroot()
+            adobe_info['version'] = root.find('''.//UpdateID''').text
             adobe_info['description'] = root.find('''.//Description/en_US''').text.replace(u'\xa0', u' ')
-        
-        munkicommon.unmountdmg(mount)
-
-# Windows Adobe Update (.zip)
-elif file_mime == 'application/zip' and file_is_local and IS_ADOBE_UPDATE:
-    
-    # Pick template based on '64bit' or '32bit' in file_path
-    if any(x in file_path for x in ['64Bit', '64bit', 'X64', 'x64']):
-        template = env.get_template('ccupdatewindows64.bes')
-    elif any(x in file_path for x in ['32Bit', '32bit']):
-        template = env.get_template('ccupdatewindows32.bes')
-    else:
-        template = env.get_template('ccupdatewindows.bes')
-
-    zf = zipfile.ZipFile(file_path, 'r')
-    extractdir = os.path.join(tempfile.gettempdir(), file_name_noextension)
-
-    for name in zf.namelist():
-        if not name.endswith('.zip') and not name.endswith('.exe'):
-            if name.endswith('Setup.xml') or name.endswith('setup.xml'):
-                setup_xml = name
-            elif name.endswith('UpdateManifest.xml'):
-                update_manifest = name
             
-            (dirname, filename) = os.path.split(name)
-            zf.extract(name, extractdir)
-
-    adobe_info = adobeutils.getAdobeSetupInfo(extractdir)
-    adobe_info['adobepatchinstaller'] = 'AdobePatchInstaller.exe'
-
-    try:
-        with open(os.path.join(extractdir, setup_xml), 'r') as setupfile:
-            root = ET.parse(setupfile).getroot()
-            adobe_info['display_name'] = root.find('''.//Media/Volume/Name''').text
-    except AttributeError:
-        pass # Can't find display name, so we'll get it from UpdateManifest next
+            # Failed to get display_name from Setup.xml, so look in UpdateManifest
+            if not adobe_info.get('display_name'):
+                adobe_info['display_name'] = root.find('''.//DisplayName/en_US''').text
     
-    with open(os.path.join(extractdir, update_manifest), 'r') as manifestfile:
-        root = ET.parse(manifestfile).getroot()
-        adobe_info['version'] = root.find('''.//UpdateID''').text
-        adobe_info['description'] = root.find('''.//Description/en_US''').text.replace(u'\xa0', u' ')
+        shutil.rmtree(extractdir)
+    
+    # Process Adobe Update
+    if 'adobe_info' in locals():
         
-        # Failed to get display_name from Setup.xml, so look in UpdateManifest
-        if not adobe_info.get('display_name'):
-            adobe_info['display_name'] = root.find('''.//DisplayName/en_US''').text
-
-    shutil.rmtree(extractdir)
-
-# Process Adobe Update
-if 'adobe_info' in locals():
+        # Get direct download link from url file
+        with open('.'.join([file_path, 'url']), 'r') as url_file:
+            adobe_info['url'] = url_file.readline()
+        
+        # Trim description
+        if ':' in adobe_info['description']:
+            adobe_info['description'] = adobe_info['description'].split(' : ', 1)[-1]
+        
+        # Sanitize and workaround Adobe naming inconsistency
+        adobe_info['name'] = ''.join(adobe_info['display_name'].split('.')[0])
+        if 'Flash' in adobe_info['name'] and 'Professional ' in adobe_info['name']:
+            adobe_info['name'] = adobe_info['name'].replace('Professional ', '')
     
-    # Get direct download link from url file
-    with open('.'.join([file_path, 'url']), 'r') as url_file:
-        adobe_info['url'] = url_file.readline()
+        # Determine base version
+        adobe_info['base_version'] = "%s.0.0" % adobe_info['version'].split('.')[0]
     
-    # Trim description
-    if ':' in adobe_info['description']:
-        adobe_info['description'] = adobe_info['description'].split(' : ', 1)[-1]
+        adobe_info['base_file_name'] = base_file_name
+        adobe_info.update(get_env_source_mime_data())
+        adobe_info.update(get_sha1_size(file_path))
     
-    # Sanitize and workaround Adobe naming inconsistency
-    adobe_info['name'] = ''.join(adobe_info['display_name'].split('.')[0])
-    if 'Flash' in adobe_info['name'] and 'Professional ' in adobe_info['name']:
-        adobe_info['name'] = adobe_info['name'].replace('Professional ', '')
-
-    # Determine base version
-    adobe_info['base_version'] = "%s.0.0" % adobe_info['version'].split('.')[0]
-
-    adobe_info['base_file_name'] = base_file_name
-    adobe_info.update(get_env_source_mime_data())
-    adobe_info.update(get_sha1_size(file_path))
-
-    # Render and POST new task to console site
-    new_task = B.post('tasks/custom/SysManDev', template.render(**adobe_info))
+        # Render and POST new task to console site
+        new_task = B.post('tasks/custom/SysManDev', template.render(**adobe_info))
 
 # Reporting Output
 if 'new_task' in locals(): 
